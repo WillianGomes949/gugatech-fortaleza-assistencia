ce3845428f82de50ae6619fea74f5608b8c321b0
Willian Gomes <146601824+WillianGomes949@users.noreply.github.com>
2025-11-16 21:33:23 -0300

    aa

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,36 +1,20 @@
-// lib/sanity.client.ts
-import { createClient } from "next-sanity";
-
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-05-03";
+import { createClient } from '@sanity/client'
 
 // Validação simples para garantir que as variáveis de ambiente estão carregadas
 if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
-  throw new Error("Missing NEXT_PUBLIC_SANITY_PROJECT_ID");
+  throw new Error('Missing NEXT_PUBLIC_SANITY_PROJECT_ID')
 }
 if (!process.env.NEXT_PUBLIC_SANITY_DATASET) {
-  throw new Error("Missing NEXT_PUBLIC_SANITY_DATASET");
+  throw new Error('Missing NEXT_PUBLIC_SANITY_DATASET')
 }
 if (!process.env.SANITY_API_TOKEN) {
-  throw new Error("Missing NEXT_SANITY_API_TOKEN");
+  throw new Error('Missing SANITY_API_TOKEN')
 }
 
-// Client PÚBLICO (para ler dados)
 export const sanityClient = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Mude para false se usar getStaticProps/ISR
-});
-
-// Client PRIVADO (para escrever dados)
-// Note o token!
-export const sanityWriteClient = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: false,
-  token: process.env.NEXT_SANITY_API_TOKEN,
-});
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: '2024-05-01', 
+  useCdn: false, 
+  token: process.env.SANITY_API_TOKEN,
+})
